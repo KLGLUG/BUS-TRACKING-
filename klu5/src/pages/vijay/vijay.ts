cb74a2bf144f36815a9ff34405073487d3d3e4bb
Manideep2897 <manchodumani123@gmail.com>
2018-04-27 14:30:10 +0530

    Add files via upload
    complete retrive to map

--- klu5/src/pages/vijay/vijay.ts
+++ klu5/src/pages/vijay/vijay.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams,Platform } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { AgmCoreModule } from '@agm/core';
+import { Geolocation } from '@ionic-native/geolocation';
 /**
  * Generated class for the VijayPage page.
  *
@@ -14,12 +16,53 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'vijay.html',
 })
 export class VijayPage {
+  coords:any;
+  location:any;
+latitude:any;
+LATITUDE:any;
+help:any;
+ite:any;
+  public items : Array<any> = [];
+  constructor(public navCtrl: NavController, private geolocation: Geolocation,public platform:Platform, public http:HttpClient,public navParams: NavParams) {
+    
+  }
+  options={
+    colorl:'#ASDF96',
+    fontfamilyl:'',
+    fontsizel:'14px',
+    fontweightl:'bold',
+    textl:'B',
+   }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+ionViewWillEnter() : void
+   {
+      this.load();
+     
+   }
+
+
+
+  load() : void
+  {
+    
+      let LATITUDE: any
+     this.http
+     .get('http://192.168.1.1/mani/retrive.php')
+     .subscribe((data : any) =>
+     {
+        console.log(data);
+       this.items=data;
+      let vamsi=this.items;
+        console.log(vamsi.toString() );
+     },
+     (error : any) =>
+     {
+        console.dir(error);
+     });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad VijayPage');
-  }
+  
+} 
+  
 
-}
+
